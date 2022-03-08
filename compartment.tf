7cdd7c28f65ca98be61896a768c869252a3f7b6d
Meghashree N <meghashreetendulkar@gmail.com>
2022-03-08 09:28:04 +0530

    Create compartment.tf

---
+++ compartment.tf
@@ -0,0 +1,15 @@
+#resource block for compartment
+
+resource "oci_identity_compartment" "compartment" {
+  # provider       = oci.home
+  compartment_id = var.compartment_ocid
+  description    = "create a comparment within your oci compartment"
+  name           = "Mashr_lab_compartment"
+  #region  = "${var.home_region}"
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [oci_identity_compartment.compartment]
+
+  create_duration = "60s"
+}
