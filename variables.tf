21f759b2b638ee6f4074fbe1e90fa9e70d3e3513
Meghashree N <meghashreetendulkar@gmail.com>
2022-03-08 10:41:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -65,11 +65,11 @@ variable "flex_instance_image_ocid" {
 }
 
 variable "ssh_public_key" {
-default = var.ssh_public_key
+default = file("~/.ssh/id_rsa.pub")
 }
 
 variable "ssh_private_key" {
-default = var.ssh_private_key
+default = file("~/.ssh/id_rsa")
 }
 
 #variable "instance_private_ip"{
