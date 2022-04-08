0a0eb6799526bfc3c90c479606fdbb38ba68341e
Meghashree N <meghashreetendulkar@gmail.com>
2022-04-08 12:40:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,20 @@
 # define provider configuration
 provider "oci" {
+  alias = "home"
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
-  region           = var.region
+  region           = var.region1
+}
+
+provider "oci" {
+  alias = "region2"
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region2
 }
 
 data "oci_identity_availability_domain" "ad" {
