31ad6c23702267f2dae0d940b78e162aa5a662be
Meghashree N <meghashreetendulkar@gmail.com>
2022-04-08 12:42:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -28,7 +28,8 @@ variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
 variable "compartment_ocid" {}
-variable "region" {}
+variable "region1" {}
+variable "region2" {}
 
 # variables for vcn networking components
 variable "vcn_cidr_block"{
